f4cd6a6bbc89c213aab6b7e6d195a846092a865f
Elias I Roussos <eliasisaiah@outlook.com>
2021-05-19 16:56:08 -0400

    messy spaghetti to make paths for things

--- app.js
+++ app.js
@@ -4,44 +4,68 @@ const fsPromises = require('fs').promises
 const sharp = require('sharp');
 const inquirer = require('inquirer');
 
-const allowedTypes = [".jpg"]
-let sizes = [1000, 500, 200]
+const allowedTypes = [".jpg", ".tif"]
+// let sizes = [1000, 500, 200];
+let sizes = [3840, 2560, 1920, 1366, 1024];
 
 ResizeImage = (imagePath) => {
-    imagePath = path.join("images", imagePath);
-    console.log(`imagePath: ${imagePath}`);
     sizes.map((size) => {
 
-        sharp(imagePath)
-            .toFormat("jpeg")
+        let newImagePath = path.join("images", "resized", `RESIZED_images_${size}`, `RESIZED_${size}_${path.parse(imagePath).name}.jpg`)
+        console.log(`imagePath: ${imagePath}`);
+        console.log(`newImagePath: ${newImagePath}`);
+
+        // sharp(path.join("originals", `${imagePath}`))
+        sharp(path.join("images", "originals", imagePath))
             .resize(size)
             .jpeg()
-            .toFile(path.join("RESIZED_images", `RESIZED_${size}_${path.basename(imagePath)}`))
-            .then((data) => {
-                console.log("data", data);
-            })
-            .catch(err => {
-                console.log(`error: ${err}`);
-            })
+            // .toBuffer()
+            .toFile(newImagePath)
+            .then(info => { console.log("info", info) })
+            .catch(err => { console.log("error:", err); })
     })
 }
 
 PromiseGetImages = async function () {
     try {
         try {
-            const fileStats = await fsPromises.stat("./images");
-            console.log(fileStats);
+            await fsPromises.stat(path.join("images"));
         } catch (error) {
             switch (error.code) {
                 case "ENOENT":
-                    await fsPromises.mkdir("./images");
+                    await fsPromises.mkdir(path.join("images"));
                     break;
                 default:
                     throw error;
             }
         }
-        const files = await fsPromises.readdir("./images");
+        try {
+            await fsPromises.stat(path.join("images", "originals"));
+        } catch (error) {
+            switch (error.code) {
+                case "ENOENT":
+                    await fsPromises.mkdir(path.join("images", "originals"));
+                    break;
+                default:
+                    throw error;
+            }
+        }
+        try {
+            await fsPromises.stat(path.join("images", "resized"));
+        } catch (error) {
+            switch (error.code) {
+                case "ENOENT":
+                    for (size of sizes) {
+                        await fsPromises.mkdir(path.join("images", "resized", `RESIZED_images_${size}`), { recursive: true });
+                    }
+                    break;
+                default:
+                    throw error;
+            }
+        }
+        const files = await fsPromises.readdir(path.join("images", "originals"));
         files.map(image => {
+            console.log("path parse name", path.parse(image).name);
             allowedTypes.includes(path.extname(image).toLowerCase()) ? ResizeImage(image) : console.log(`${image} is not allowed type`);
         })
     } catch (error) { console.log(error); }
